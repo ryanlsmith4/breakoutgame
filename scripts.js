908687b6233d740dae2961fa553a62d79239ef74
Ryan Smith <ryan.l.smith4@gmail.com>
2019-01-22 14:57:07 -0800

    bounce off the walls

--- scripts.js
+++ scripts.js
@@ -2,20 +2,26 @@
 const canvas = document.getElementById('canvas');
 const ctx = canvas.getContext('2d');
 
-// RECT
-ctx.beginPath();
-//  FRM L,FRM,T    w ,  h
-ctx.rect(20, 40, 50, 50);
-ctx.fillStyle = '#FF0000';
-ctx.fill();
-ctx.closePath();
+let x = canvas.width/2;
+let y = canvas.height - 30;
+let dx = 2;
+let dy = -2;
 
-// BALL
-ctx.beginPath();
-//x,y arcs center, R, start, end,   clockwise, true is counter
-ctx.arc(240, 160, 20, 0, Math.PI*2, false);
-// ctx.strokeStyle = 'green';
-// ctx.stroke();
-ctx.fillStyle = 'green';
-ctx.fill();
-ctx.closePath();
+
+const drawBall = () => {
+    ctx.beginPath();
+    ctx.arc(x, y, 10, 0, Math.PI*2);
+    ctx.fillStyle = '#0095DD';
+    ctx.fill();
+    ctx.closePath();
+
+}
+
+const draw = () => {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    drawBall();
+    x += dx;
+    y += dy;
+}
+
+setInterval(draw, 10);

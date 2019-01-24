e6d5d8691d123e7f33e00a3aad3032a9f964a9e2
Ryan Smith <ryan.l.smith4@gmail.com>
2019-01-24 15:46:34 -0800

    finished breakout.js per basics can added addit features

--- scripts.js
+++ scripts.js
@@ -2,35 +2,211 @@
 const canvas = document.getElementById('canvas');
 const ctx = canvas.getContext('2d');
 
+let score = 0;
+let lives = 3;
+
 // declare Ball variables
-let x = canvas.width/2;
+let x = canvas.width / 2;
 let y = canvas.height - 30;
 let dx = 2;
 let dy = -2;
-
 let ballRadius = 10;
+
+// declare paddle variables
+let paddleHeight = 10;
+let paddleWidth = 75;
+let paddleX = (canvas.width - paddleWidth) / 2;
+let rightPressed = false;
+let leftPressed = false;
+
+// declare brick variables
+let brickRowCount = 3;
+let brickColumnCount = 5;
+let brickWidth = 75;
+let brickHeight = 20;
+let brickPadding = 10;
+let brickOffsetTop = 30;
+let brickOffsetLeft = 30;
+
+// Logic to create bricks
+let bricks = [];
+for (var c = 0; c < brickColumnCount; c++) {
+    bricks[c] = [];
+    for (var r = 0; r < brickRowCount; r++) {
+        bricks[c][r] = {
+            x: 0,
+            y: 0,
+            status: 1
+        };
+    }
+};
+
+// KeyEvent Listeners
+document.addEventListener('keydown', keyDownHandler, false);
+document.addEventListener('keyup', keyUpHandler, false);
+document.addEventListener('mousemove', mouseMoveHandler, false);
+
+// These ES6 functions don't work this event listener... Wondering why?
+// =============================================================================
+// let keyDownHandler = (e) => {
+//     if(e.key == 'Right' || e.key == 'ArrowRight'){
+//         rightPressed = true;
+//     }
+//     else if(e.key == 'Left' || e.key == 'ArrowLeft'){
+//         leftPressed = true;
+//     }
+// };
+//
+// let keyUpHandler = (e) => {
+//     if(e.key == 'Right' || e.key == 'ArrowRight') {
+//         rightPressed = false;
+//     }
+//     else if(e.key == 'Left' || e.key == 'ArrowLeft'){
+//         leftPressed = false;
+//     }
+// };
+
+// Functions to handle the events
+
+function keyDownHandler(e) {
+    if (e.key == "Right" || e.key == "ArrowRight") {
+        rightPressed = true;
+    } else if (e.key == "Left" || e.key == "ArrowLeft") {
+        leftPressed = true;
+    }
+};
+
+function keyUpHandler(e) {
+    if (e.key == "Right" || e.key == "ArrowRight") {
+        rightPressed = false;
+    } else if (e.key == "Left" || e.key == "ArrowLeft") {
+        leftPressed = false;
+    }
+};
+
+function mouseMoveHandler(e) {
+    var relativeX = e.clientX - canvas.offsetLeft;
+    if(relativeX > 0 && relativeX < canvas.width) {
+        paddleX = relativeX - paddleWidth/2
+    }
+};
+
+//  Functions for BreakoutJS
+// =============================================================================
+var collisionDetection = () => {
+    for (var c = 0; c < brickColumnCount; c++) {
+        for (var r = 0; r < brickRowCount; r++) {
+            var b = bricks[c][r];
+            if (b.status == 1) {
+                if (x > b.x && x < b.x + brickWidth && y > b.y && y < b.y + brickHeight) {
+                    dy = -dy;
+                    b.status = 0;
+                    score++;
+                    if(score == brickRowCount*brickColumnCount){
+                        alert("YOU WIN CONGRATULATIONS");
+                        document.location.reload();
+                    }
+                }
+            }
+        }
+    }
+};
 
 const drawBall = () => {
     ctx.beginPath();
-    ctx.arc(x, y, ballRadius, 0, Math.PI*2);
+    ctx.arc(x, y, ballRadius, 0, Math.PI * 2);
     ctx.fillStyle = '#0095DD';
     ctx.fill();
     ctx.closePath();
 
-}
+};
+
+const drawPaddle = () => {
+    ctx.beginPath();
+    ctx.rect(paddleX, canvas.height - paddleHeight, paddleWidth, paddleHeight);
+    ctx.fillStyle = '0095DD';
+    ctx.fill();
+    ctx.closePath();
+
+};
+
+const drawBricks = () => {
+    for (var c = 0; c < brickColumnCount; c++) {
+        for (var r = 0; r < brickRowCount; r++) {
+            if (bricks[c][r].status == 1) {
+                var brickX = (c * (brickWidth + brickPadding)) + brickOffsetLeft;
+                var brickY = (r * (brickHeight + brickPadding)) + brickOffsetTop;
+                bricks[c][r].x = brickX;
+                bricks[c][r].y = brickY;
+                ctx.beginPath();
+                ctx.rect(brickX, brickY, brickWidth, brickHeight);
+                ctx.fillStyle = '#0095DD';
+                ctx.fill();
+                ctx.closePath();
+            }
+        }
+    }
+};
+
+const drawScore = () => {
+    ctx.font ='16px Arial';
+    ctx.fillStyle = '#0095DD'
+    ctx.fillText("Score: "+ score, 8, 20)
+};
+
+const drawLives = () => {
+    ctx.font = '16px Arial';
+    ctx.fillStyle = '#0095DD';
+    ctx.fillText('Lives: '+ lives, canvas.width-65, 20)
+};
 
 const draw = () => {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
+    drawBricks();
     drawBall();
-    x += dx;
-    y += dy;
-    if(y + dy > canvas.height - ballRadius || y + dy < ballRadius) {
-        dy = -dy;
-    }
+    drawPaddle();
+    collisionDetection()
+    drawScore();
+    drawLives();
 
-    if(x + dx > canvas.width - ballRadius || x + dx < ballRadius) {
+    if (x + dx > canvas.width - ballRadius || x + dx < ballRadius) {
         dx = -dx;
     }
-}
 
-setInterval(draw, 10);
+    if (y + dy < ballRadius) {
+        dy = -dy;
+    } else if (y + dy > canvas.height - ballRadius) {
+        if (x > paddleX && x < paddleX + paddleWidth) {
+            dy = -dy;
+        } else {
+            lives--;
+            if(!lives) {
+                alert('GAME OVER');
+                document.location.reload();
+            }
+            else {
+                x = canvas.width/2;
+                y = canvas.height - 30;
+                dx = 3;
+                dy = -3;
+                paddleX = (canvas.width - paddleWidth)
+            }
+        }
+    }
+    if (rightPressed && paddleX < canvas.width - paddleWidth) {
+        paddleX += 7;
+    } else if (leftPressed && paddleX > 0) {
+        paddleX -= 7;
+    }
+    x += dx;
+    y += dy;
+    requestAnimationFrame(draw);
+};
+
+
+
+
+
+
+
+draw();

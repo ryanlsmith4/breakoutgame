4afabf6c2b004508d10df7ccfe2da3951317e28d
Ryan Smith <ryan.l.smith4@gmail.com>
2019-01-31 12:05:40 -0800

    Changed to OOps

--- scripts.js
+++ scripts.js
@@ -8,21 +8,86 @@
 // score
 // lives
 // game
-class Game {
-  constructor() {
-    this.canvas = document.getElementById('canvas')
-    this.ball = new Ball(this.canvas.width/2, this.canvas.height - 30);
+let rightPressed = false;
+let leftPressed = false;
 
+class Game {
+  constructor(dx = 4, dy = -4) {
+    this.canvas = document.getElementById('canvas');
+    this.ctx = this.canvas.getContext('2d');
+    this.dx = dx;
+    this.dy = dy;
+    this.rightPressed = false;
+    this.leftPressed = false;
+    this.score = new Score(this.canvas.width);
+    this.lives = new Lives(this.canvas.width);
+    this.paddle = new Paddle((this.canvas.width - 75) / 2, this.canvas);
+    this.ball = new Ball(this.canvas.width / 2,
+    this.canvas.height - 30);
+    this.bricks = new Bricks();
+
+  }
+
+  run() {
+    this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
+    this.bricks.render(this.ctx);
+    this.ball.render(this.ctx);
+    this.paddle.render(this.ctx, canvas);
+    this.score.render(this.ctx);
+    this.lives.render(this.ctx);
+    this.ball.move();
+    this.collisionDetection();
+    requestAnimationFrame(this.run.bind(this));
+  }
+
+  collisionDetection() {
+    if (this.ball.x < this.ball.radius || this.ball.x > this.canvas.width - this.ball.radius) {
+      this.ball.dx *= -1;
+    }
+    if (this.ball.y < this.ball.radius) {
+      this.ball.dy *= -1;
+    }
+    if (this.ball.y > this.canvas.height - this.ball.radius) {
+      this.lives.loseLife();
+      this.ball.reset();
+      if (this.lives.lives === 0) {
+        alert('You Blew IT!!!!!');
+        document.location.reload();
+      }
+    }
+    if (this.score.score === 15) {
+      alert('You Win!~!!');
+      document.location.reload();
+    }
+    if (this.ball.y > this.canvas.height - this.ball.radius - this.paddle.height
+       && this.ball.x < this.paddle.x + this.paddle.width && this.ball.x > this.paddle.x) {
+      this.ball.dy *= -1;
+    }
+    for (let c = 0; c < this.bricks.columnCount; c += 1) {
+      for (let r = 0; r < this.bricks.rowCount; r += 1) {
+        const brick = this.bricks.bricks[c][r];
+        if (brick.status === 1 && this.ball.x > brick.x
+          && this.ball.x < brick.x + brick.width) {
+          if (this.ball.y + this.ball.radius > brick.y
+            && this.ball.y - this.ball.radius < brick.y + brick.height) {
+            this.ball.dy *= -1;
+            brick.status = 0;
+            this.score.update(1);
+          }
+        }
+      }
+    }
   }
 }
 
-
 class Ball {
   constructor(x, y, radius = 10) {
+    this.originalX = x;
+    this.originalY = y;
     this.x = x;
     this.y = y;
     this.dx = 2;
-    this.yd = -2;
+    this.dy = -2;
     this.radius = radius;
   }
 
@@ -38,40 +103,112 @@ class Ball {
     ctx.fill();
     ctx.closePath();
   }
+
+  reset() {
+    this.x = this.originalX;
+    this.y = this.originalY;
+    this.dx = 2;
+    this.dy = -2;
+  }
 }
 
 class Paddle {
-  constructor(x, y, Height = 10, Width = 75, color = red) {
+  constructor(x, canvas, height = 10, width = 75, color = 'red') {
     this.x = x;
-    this.y = y;
-    this.Height = Height;
-    this.Width = Width;
+    this.y = 10;
+    this.height = height;
+    this.width = width;
     this.color = color;
+    this.canvas = canvas;
   }
 
   render(ctx) {
     ctx.beginPath();
-    ctx.rect(this.x, game.canvas.height - this.height, this.width, this.height);
+    ctx.rect(this.x, this.canvas.height - this.height, this.width, this.height);
+    ctx.fillStyle = this.color;
+    ctx.fill();
+    ctx.closePath();
+    if (leftPressed) {
+      this.moveLeft();
+    }
+    if (rightPressed) {
+      this.moveRight();
+    }
+  }
+
+  moveLeft() {
+
+    if (this.x > 0) {
+      this.x -= 7;
+    }
+  }
+
+  moveRight() {
+    if (this.x + this.width < this.canvas.width) {
+      this.x += 7;
+    }
+  }
+}
+
+class Brick {
+  constructor(c = 0, r = 0, x = 100, y = 100, color = '#0095DD') {
+    this.c = c;
+    this.r = r;
+    this.color = color;
+    this.x = x
+    this.y = y
+    this.status = 1;
+    this.width = 75;
+    this.height = 20;
+  }
+
+  render(ctx) {
+    ctx.beginPath();
+    ctx.rect(this.x, this.y, this.width, this.height);
+    const red = 255 * (this.c / this.r);
+    const green = 255 * (this.r / this.c);
+    const blue = (this.c * this.r);
+    ctx.fillStyle = `rgb(${red}, ${green}, ${blue})`;
     ctx.fill();
     ctx.closePath();
   }
 
-  moveLeft() {
-    this.x - 7;
-  }
-
-  moveRight() {
-    this.x + 7;
-  }
 }
 
-//WORK IN PROGRESS
-class Brick {
-  constructor() {
-    this.x = 0;
-    this.y = 0;
-    this.status = 1;
+class Bricks {
+  constructor(row = 3, column = 5) {
+    this.bricks = []
+    this.rowCount = row;
+    this.columnCount = column;
+    this.padding = 10;
+    this.offsetTop = 30;
+    this.offsetLeft = 30;
+    this.make();
   }
+
+  make() {
+    for (let c = 0; c < this.columnCount; c++) {
+      this.bricks[c] = [];
+      for (let r = 0; r < this.rowCount; r++) {
+        this.bricks[c][r] = new Brick(c, r)
+      }
+    }
+  }
+
+  render(ctx) {
+    for (let c = 0; c < this.columnCount; c++) {
+      for (let r = 0; r < this.rowCount; r++) {
+        if (this.bricks[c][r].status == 1) {
+          const brickX = (c * (this.bricks[c][r].width + this.padding)) + this.offsetLeft;
+          const brickY = (r * (this.bricks[c][r].height + this.padding)) + this.offsetTop;
+          this.bricks[c][r].x = brickX;
+          this.bricks[c][r].y = brickY;
+          this.bricks[c][r].render(ctx)
+        }
+      }
+    }
+  }
+
 }
 
 class Score {
@@ -99,8 +236,8 @@ class Score {
 }
 
 class Lives {
-  constructor(color = 'Red', lives = 3, font = '16px Arial') {
-    this.x = game.width - 65;
+  constructor(width, color = 'Red', lives = 3, font = '16px Arial') {
+    this.x = width - 65;
     this.y = 20;
     this.color = color;
     this.lives = lives;
@@ -114,35 +251,21 @@ class Lives {
   }
 
   loseLife() {
-    this.life - 1;
+    this.lives -= 1;
   }
 
-  reset() {
-    this.life = lives;
+  reset(lives = 0) {
+    this.lives = lives;
   }
 }
 
-class Background {
-  this.background = document.getElementById('back')
-  this.
-}
+// class Background {
+//   this.background = document.getElementById('back')
+//   this.
+// }
 
 
-
-
-
-
-
-
-
-
-
-
-
-const canvas = document.getElementById('canvas');
-const ctx = canvas.getContext('2d');
-
-const ball = new Ball(canvas.width/2, canvas.height - 30)
+const ball = new Ball(canvas.width / 2, canvas.height - 30)
 
 let score = 0;
 let lives = 3;
@@ -158,8 +281,7 @@ const ballRadius = 10;
 const paddleHeight = 10;
 const paddleWidth = 75;
 let paddleX = (canvas.width - paddleWidth) / 2;
-let rightPressed = false;
-let leftPressed = false;
+
 
 // declare brick variables
 const brickRowCount = 3;
@@ -353,5 +475,8 @@ const draw = () => {
   requestAnimationFrame(draw);
 };
 
+const game = new Game();
+game.run();
 
-draw();
+
+// draw();
